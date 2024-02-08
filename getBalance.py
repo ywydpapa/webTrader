994c9e29cf5c942aeba9f1f8dc861ca96b647f1e
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2024-02-09 01:43:19 +0900

    로그인 화면 수정

--- getBalance.py
+++ getBalance.py
@@ -4,8 +4,4 @@ access = "AT7HeOx48ZzzRDTO5y6FA2Lmw12ufGHwWArU5xVz"
 secret = "b4Qg0uZaapuTk18Q7qESC4AQIVboTI9HAhduY4SD"
 upbit = pyupbit.Upbit(access, secret)
 
-print(upbit.get_balance("KRW-XRP"))     # KRW-BTC 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
-print(upbit.get_order("KRW-XRP",state='wait'))
-print(pyupbit.get_orderbook("KRW-XRP"))
-print(pyupbit.get_tickers())
+print(upbit.get_balances())     # KRW-BTC 조회
